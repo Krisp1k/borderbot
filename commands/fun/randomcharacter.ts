de50bbfca4028d47c3cf5423970c0c1b37ec4a56
Krisp1k <75334703+Krisp1k@users.noreply.github.com>
2022-06-27 21:42:31 +0200

    changed randomcharacter command, update configs

--- commands/fun/randomcharacter.ts
+++ commands/fun/randomcharacter.ts
@@ -3,16 +3,33 @@ import { Message } from 'discord.js'
 export default {
     callback: (message: Message, ...args:string[]) => {
 
-        const characters:string[] = [
-            "Moze","Amara","Zane","Fl4k",
-            "Zer0","Maya","Axton","Salvador",
-            "Lilith","Brick","Roland","Mordecai",
-            "TnTina","Ellie","Crazy Earl","Sir Hammerlock","Tannis","Moxxi","Marcus"
-        ]
-        let randomchar = Math.floor(Math.random() * characters.length);
+        const characters: {[key: string]: string} = {
+            "Moze": "", 
+            "Amara": "",
+            "Zane": "", 
+            "Fl4k": "", 
+            "Zer0": "", 
+            "Maya": "", 
+            "Axton": "" ,
+            "Salvador": "",
+            "Lilith": "",
+            "Brick": "" ,
+            "Roland": "",
+            "Mordecai": "",
+            "TnTina": "",
+            "Ellie": "" ,
+            "Crazy Earl": "",
+            "Sir Hammerlock": "",
+            "Tannis": "",
+            "Moxxi" : "",
+            "Marcus": ""
+        }
+
+        let randomChar = Object.keys(characters)[Math.floor(Math.random() * Object.keys(characters).length)]
+        let charImage = Object.values(characters)[Math.floor(Math.random() * Object.keys(characters).length)]
 
         message.reply({
-            content:`${message.author}, you are **${characters[randomchar]}**!`
+            content:`${message.author}, you are **${randomChar}**!`
         })
 
         console.log(args, [message.author + " : " + message.content])
